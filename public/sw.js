72ca8d3a980f2549673d484a251c0a32649aeb00
Sriharsan <bssriharsan@gmail.com>
2026-06-18 22:02:59 +0530

    ci: simplify build workflow

--- public/sw.js
+++ public/sw.js
@@ -2,21 +2,17 @@ const CACHE_NAME = "vestra-v1";
 const SHELL_ASSETS = ["/", "/offline.html"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_ASSETS)),
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key)),
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
       ),
-    ),
   );
   self.clients.claim();
 });
